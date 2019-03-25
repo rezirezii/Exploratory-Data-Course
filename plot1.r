f24f3b9fc09deb824814112a8f87ae825dd6549c
rezi <rezvan.rafiei@gmail.com>
2019-03-25 13:14:10 -0400

    changed png files

--- plot1.r
+++ plot1.r
@@ -2,5 +2,5 @@ power <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strin
 power1 <- subset(power, Date %in% c("1/2/2007","2/2/2007"))
 power1$Date <- as.Date(power1$Date, format="%d/%m/%Y")
 hist(power1$Global_active_power, main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-png("plot1.png", width=480, height=480)
+dev.copy(png, file="plot1.png", width=480, height=480)
 dev.off()
